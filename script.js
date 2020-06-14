178efc37da075217fd0f77ad6d9e40cd6b796902
Giovanna Aveiro <to.gio@pm.me>
2020-06-14 22:31:33 +0100

    Add styling to aside element on desktop mode

--- script.js
+++ script.js
@@ -2,15 +2,13 @@ const menuIcon = document.querySelector('.navbar-icon')
 const toggleMenu = document.querySelector('.menu')
 const toggleNavbar = document.querySelector('.navbar')
 const toggleSocials = document.querySelector('.socials')
-const toggleItemOne = document.querySelector('.navbar-item-one')
-const toggleItemTwo = document.querySelector('.navbar-item-two')
+
 
 menuIcon.addEventListener('click', (event) => {
   toggleMenu.classList.toggle('toggle');
   toggleMenu.classList.toggle('toggle-menu');
   toggleNavbar.classList.toggle('toggle-navbar');
   toggleSocials.classList.toggle('toggle-socials');
-  toggleItemOne.classList.toggle('toggle-item');
-  toggleItemTwo.classList.toggle('toggle-item');
+
 
 })
