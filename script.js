7f9a7565ef7508ce4b014c40875482b02f8949ef
Giovanna Aveiro <to.gio@pm.me>
2020-06-24 00:48:41 +0100

    Add final video and change menu icon to triangle Relates #9

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const body = document.querySelector('.body')
 
 menuIcon.addEventListener('click', (event) => {
   toggleMenu.classList.toggle('d-none');
+  menuIcon.classList.toggle('toggle-navbar-icon');
   toggleMenu.classList.toggle('toggle-menu');
   toggleNavbar.classList.toggle('toggle-navbar');
   toggleSocials.classList.toggle('toggle-socials');
