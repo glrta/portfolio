a0814ba4cab664030327412719bcbafe78718dcf
Giovanna Aveiro <to.gio@pm.me>
2020-08-23 15:03:52 +0100

    Change 'cv' link to open new window

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const menuIcon = document.querySelector('.navbar-icon')
-const menu = document.querySelector('.menu')
-const toggleNavbar = document.querySelector('.navbar')
-const toggleSocials = document.querySelector('.socials')
-const body = document.querySelector('.body')
-const menuItemAbout = document.querySelector('#menu-about')
-const menuItemWork = document.querySelector('#menu-work')
+const menuIcon = document.querySelector('.navbar-icon');
+const menu = document.querySelector('.menu');
+const toggleNavbar = document.querySelector('.navbar');
+const toggleSocials = document.querySelector('.socials');
+const body = document.querySelector('.body');
+const menuItem = document.querySelectorAll('#menu');
+const download = document.querySelectorAll('.download');
 
 const toggleMenu = () => {
   menu.classList.toggle('d-none');
@@ -12,26 +12,26 @@ const toggleMenu = () => {
   menu.classList.toggle('toggle-menu');
   toggleNavbar.classList.toggle('toggle-navbar');
   toggleSocials.classList.toggle('toggle-socials');
-  controlOverflow()
-}
+  controlOverflow();
+};
 const controlOverflow = () => {
   if (menu.classList.contains('toggle-menu')) {
-    body.classList.add('lockScroll')
-  } else (
-    body.classList.remove('lockScroll')
-  )
-}
+    body.classList.add('lockScroll');
+  } else body.classList.remove('lockScroll');
+};
 
-menuIcon.addEventListener('click', toggleMenu)
+menuIcon.addEventListener('click', toggleMenu);
 
-menuItemAbout.addEventListener('click', (event) => {
-  if (menu.classList.contains('toggle-menu')) {
-    toggleMenu()
-  }
-})
+menuItem.forEach((item) => {
+  item.addEventListener('click', (event) => {
+    if (menu.classList.contains('toggle-menu')) {
+      toggleMenu();
+    }
+  });
+});
 
-menuItemWork.addEventListener('click', (event) => {
-  if (menu.classList.contains('toggle-menu')) {
-    toggleMenu()
-  }
-})
+download.forEach((item) => {
+  item.addEventListener('click', () => {
+    window.open('./assets/giovanna-aveiro_cv.pdf');
+  });
+});
